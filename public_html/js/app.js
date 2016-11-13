d4309c56f1af74cea9a0c2efae9008dcf20cbc76
Andrew Yeonsoo Han <drewhan90@gmail.com>
2016-11-12 17:52:01 -0800

    index styling update
    css update
    google maps & api

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+function initMap() {
+	/// Home Coordinates
+	var home = {lat: 49.282824, lng: -123.115143}; 
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: home
+	});
+	/// Map Marker
+	var marker = new google.maps.Marker({
+		position: home,
+		map: map
+	});
+};
+
