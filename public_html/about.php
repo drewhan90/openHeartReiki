0b3999c132f2adb7e3ddfb6bd3a4771c1eb43e0a
Andrew Yeonsoo Han <drewhan90@gmail.com>
2016-11-20 23:04:06 -0800

    about page update v1.1
    sticky bar, smooth scrolling
    contents

--- public_html/about.php
+++ public_html/about.php
@@ -10,15 +10,28 @@
 <body id="about">
 	<!-- HEADER + NAV MENU -->
 	<?php include 'partials/header.php' ?>
+	<!-- STICKY HEADER -->
+	<div data-sticky-container>
+	  <div class="title-bar" data-sticky data-options="marginTop:0;" style="width:100%">
+	    <div class="section-list">
+	    	<ul class="vertical small-horizontal medium-horizontal menu">
+				<li class="about"><a href="#reiki">about reiki</a></li>
+				<li class="services"><a href="#benefit">benefits of reiki</a></li>
+				<li class="services"><a href="#history">the Usui System of Reiki</a></li>
+				<li class="services"><a href="#aneesha">about aneesha</a></li>
+			</ul>
+	    </div>
+	  </div>
+	</div>
 	<!-- Static Content: Benifits of Reiki -->
-	<section class="reiki">
+	<section id="reiki" class="reiki">
 		<div class="content">
 			<div class="title">
 				<h1>about reiki</h1>
 				<span class="divider">divider</span>
 			</div>
 			<div class="">
-				<img src="" alt="">
+				<img src="img/about/reiki-treatment.jpg" alt="">
 			</div>
 			<p>
 				“Reiki is unfathomable, immeasurable, and being a universal life force, it is incomprehensible to man. Yet, every single living being is receiving its blessings daily, awake and asleep.” <br> - Hawayo Takata, Reiki Grand Master 
@@ -33,7 +46,7 @@
 			</div>
 		</div>
 	</section>
-	<section class="benefits">
+	<section id="benefit" class="benefits secondary-bg">
 		<div class="content">
 			<div class="title">
 				<h1>benefits of reiki</h1>
@@ -42,38 +55,40 @@
 			<p>
 				Here are some benefits that people report having after the a reiki treatment: 
 			</p>
-			<ul class="medium-6 columns">
-				<li>peace of mind</li>
-				<li>promotes sleep</li>
-				<li>relaxation of body and mind</li>
-				<li>chronic pain and discomfort relieve</li>
-				<li>balance energies in the body</li>
+			<div class="list">
+				<ul class="medium-6 columns">
+					<li>peace of mind</li>
+					<li>promotes sleep</li>
+					<li>relaxation of body and mind</li>
+					<li>chronic pain and discomfort relieve</li>
+					<li>balance energies in the body</li>
 
-				<li>relaxes tight muscles</li>
-				<li>easies tension and stress</li>
-				<li>increase energy</li>
-				<li>relieves fatigue</li>
-				<li>clears toxins</li>
-			</ul>
-			<ul class="medium-6 columns">
-				<li>promotes feeling of calmness and well-being</li>
-				<li>helps unlock suppressed feelings</li>
-				<li>soothes emotional distress</li>
-				<li>promotes a positive and peaceful outlook on life</li>
-				<li>strengthens imune system</li>
-				
-				<li>slows down aging</li>
-				<li>promotes stamina</li>
-				<li>faster recovery from meical treatements and operations</li>
-				<li>promotes positive changes in life</li>
-			</ul>
+					<li>relaxes tight muscles</li>
+					<li>easies tension and stress</li>
+					<li>increase energy</li>
+					<li>relieves fatigue</li>
+					<li>clears toxins</li>
+				</ul>
+				<ul class="list medium-6 columns">
+					<li>promotes feeling of calmness and well-being</li>
+					<li>helps unlock suppressed feelings</li>
+					<li>soothes emotional distress</li>
+					<li>promotes a positive and peaceful outlook on life</li>
+					<li>strengthens imune system</li>
+					
+					<li>slows down aging</li>
+					<li>promotes stamina</li>
+					<li>faster recovery from meical treatements and operations</li>
+					<li>promotes positive changes in life</li>
+				</ul>
+			</div>
 			<div class="end">
-				<button class="primary-btn" href="">contact for reiki treatment</button>
+				<button class="secondary-btn" href="">contact for reiki treatment</button>
 				<span class="divider">divider</span>
 			</div>
 		</div>
 	</section>
-	<section class="history">
+	<section id="history" class="history">
 		<div class="content">
 			<div class="title">
 				<h1>the Usui System of Reiki</h1>
@@ -94,6 +109,40 @@
 				</div>
 			</div>
 		</section>
+	<section id="aneesha" class="sasha">
+		<div class="content">
+			<div class="title">
+				<h1>about Aneesha</h1>
+				<span class="divider">divider</span>
+			</div>
+			<div>
+				<img src="img/about/about-sasha.jpg" alt="">
+			</div>
+			<p>
+				As many of us, I had a very ordinary life of an office clerk. I lived in Russia and was doing okay in this busy business world. Although, my life was very stable, I felt empty, useless and unhappy. I was angry at people around me, very often feeling sick or depressed. My only joy was the passion to travel and be around interesting people in the places where I can learn something new. This curiosity to see the world and learn more about it brought me to Vancouver for a short trip and made me stay here for a new chapter of my journey. My spiritual parth opened up on its fullest in Canada.
+				<br><br>
+				I was lucky to meet my teacher; dear friend and Reiki Master the second day of my arrival in 2011 and began a deep healing for my body and my soul. I got introduced to Reiki first and experienced it’s profound healing power right away, which helped me to manage my daily pain from the chronic illness I had, lightened my mood and opened my heart and eyes to be in a present moment to enjoy every moment in its beauty.
+				<br><br>
+				Shortly after, my self-exploration journey continued with meditation. Osho dynamic mediations allowed me to open up my repressed feelings and look into suppressed emotions, fear, anger, childhood traumas and issues in my daily life. I have been practicing Osho meditations for several years now and went to a blessed land of India to study with an enlightened master Swami Samdarshi, who is a disciple of Osho.
+				<br><br>
+				I truly believe that we need to heal on all levels of our being; Reiki and meditations helped me to explore my emotions, transformed my mind and body, and showed me the beauty of Life and Love.
+				When I was ready for a deeper physical transformation and my body was healthy enough I received a gift of yoga.
+				I have taken Teacher Training program at "Karma Teachers" and that opened an understanding in me of the importance of movement and exploration of the connection between the physical and spiritual.
+				<br><br>
+				My longing to get to know myself better led me to look into my nature of being a woman and connect to Goddess Mother – divine power of Shakti Ma and source of creation all around us. For us women is very important to understand this divine power of feminine, compassion and love, so we are able to be confident, rooted, fearless and giving in this fast paced world.
+				I am grateful to God and Universe for this wonderful unpredictable journey called life, grateful for being able to follow my true purpose in this life to serve people and help them to transform their beings and bring them closer to light, divine source, their true selves. I am looking forward to meet you, hear your life journey and work together on how to make your journey of life happier, healthier and brighter.
+				<br><br>
+				Light and love from my open heart to yours,
+				<br>
+				Aneesha
+			</p>
+			<div class="end">
+				<button class="secondary-btn" href="">contact for reiki treatment</button>
+				<span class="divider">divider</span>
+			</div>
+		</div>
+	</section>
+	
 
 		<!-- FOOTER + TOP BTN + SCRIPT LINKS-->
 		<?php require 'partials/footer.php' ?>
