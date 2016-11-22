0448102220c0452205d36499c88491083499148b
Andrew Yeonsoo Han <drewhan90@gmail.com>
2016-11-22 11:44:10 -0800

    backend initials

---
+++ public_html/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$username = 'username';
+$password = 'password';
+$database = 'database';
+$dbconfig = mysqli_connect($host,$username,$password,$database);
+?>
--- public_html/login.php
+++ public_html/login.php
@@ -1,3 +1,23 @@
+<?php
+	include("dbconfig.php");
+	session_start();
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		// Recieved Login Information
+		$username = mysqli_real_escape_string($dbconfig,$_POST['username']);
+		$password = mysqli_real_escape_string($dbconfig,$_POST['password']);
+		$sql_query = "SELECT id FROM user WHERE username='$username' and password='$password'";
+		$result = mysqli_query($dbconfig,$sql_query);
+		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+		$count=mysqli_num_rows($result);// If result matched $username and $password, table row must be 1 row
+		if($count == 1){
+			$_SESSION['login_user']=$username;
+			header("location: cms.php");
+		}
+		else {
+			$error = "Username or Password is invalid";
+		}
+	}
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
